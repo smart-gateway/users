297962cec576da205e097715f1341b955c34fc13
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-10 08:57:38 -0800

    add ssh-key creation

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@
 # @example
 #   include users::config
 class users::config {
-  $::users::package_users.each | $username, $user_details | {
+  $::users::package_users.each | $user_name, $user_details | {
     # If managehome is not set then set it to the package default
     $manage_home = $user_details[managehome] == undef ? {
       true   => $::users::manage_home,
@@ -13,13 +13,23 @@ class users::config {
     }
 
     # Create user
-    user { $username:
+    user { $user_name:
       ensure     => $user_details[ensure],
+      comment    => $user_details[comment],
       password   => $user_details[password],
       managehome => $manage_home,
       groups     => $user_details[groups],
       shell      => $user_details[shell],
-      keys       => $user_details[keys],
+    }
+
+    # Create authorized keys for the user
+    $user_details[keys].each | $key_name, $key_details | {
+      ssh_authorized_key { $key_name:
+        ensure => $user_details[ensure],
+        user   => $user_name,
+        type   => "ssh-${key_details[type]}",
+        key    => $key_details[key],
+      }
     }
   }
 }
