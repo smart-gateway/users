2095762cf24240dd9ac0f003c6a436208353c2b7
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-10 09:38:56 -0800

    remove shell installs

--- manifests/config.pp
+++ manifests/config.pp
@@ -29,7 +29,7 @@ class users::config {
       ssh_authorized_key { $key_name:
         ensure => $user_details[ensure],
         user   => $user_name,
-        type   => "ssh-unknown",
+        type   => "ssh-rsa",
         key    => $key_details[key],
       }
     }
