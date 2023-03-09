3caf1e776fd05d711ed21253dbaa1f77bb8e43ce
Benjamin Grewell <benjamin.grewell@intel.com>
2023-03-09 10:54:22 -0800

    fix zsh dependency

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,17 +6,21 @@
 #   include users::install
 class users::install {
 
-  # # Make sure that zsh is installed
-  # package { 'users package ensures zsh is installed':
-  #   name   => 'zsh',
-  #   ensure => 'installed',
-  # }
-  #
-  # # Setup the Fish PPA
-  # apt::ppa  { 'ppa:fish-shell/release-3': }
-  # -> package { 'users package ensures fish is installed':
-  #   name   => 'fish',
-  #   ensure => 'installed',
-  # }
+  # Make sure that zsh is installed
+  if ! defined(Package['zsh']) {
+    package { 'users package ensures zsh is installed':
+      name   => 'zsh',
+      ensure => 'installed',
+    }
+  }
 
+
+  # Setup the Fish PPA
+  if ! defined(Package['fish']) {
+    apt::ppa  { 'ppa:fish-shell/release-3': }
+    -> package { 'users package ensures fish is installed':
+      name   => 'fish',
+      ensure => 'installed',
+    }
+  }
 }
